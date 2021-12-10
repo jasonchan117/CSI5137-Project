b4710285f6ef196e5392aee1db3ef4a25ef1a80f
Alienware <zchen314@uottawa.ca>
2021-12-09 23:03:09 -0500

    Building model.

--- model.py
+++ model.py
@@ -9,9 +9,13 @@ class F_HMN(nn.Module):
         self.bert =torch.hub.load('huggingface/pytorch-transformers', 'model', 'bert-base-cased')
         self.opt = opt
     def forward(self, text, token_type_ids, child_label_des):
+        # child_label_des: (12, 18)
 
 
-        child_label_des = child_label_des.repeat((self.opt.batchsize, 1, 1))
+        # (12, 768)
+        des_embed = self.bert(child_label_des, token_type_ids[0])[0]
+        # (bs, 12, 768)
+        des_embed = des_embed.repeat((self.opt.batchsize, 1, 1))
 
-        pass
+
 
