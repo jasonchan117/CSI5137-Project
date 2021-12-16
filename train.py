7de380a3eebf43a162366e46ca594e2a366c53e2
Alienware <zchen314@uottawa.ca>
2021-12-16 13:05:58 -0500

    Add BERT model for child label classification.

--- train.py
+++ train.py
@@ -29,7 +29,8 @@ def main():
     parser.add_argument('--test_freq', default=1, type=int)
     parser.add_argument('--des_ver', default=1, type=int, help='Use which version of child label description, 1 is the short version, 2 is the long version.')
     parser.add_argument('--wd', default=0., type=float, help='Weight decay.')
-    parser.add_argument('--metric', default = None, help= 'The way to print metric for this multi classification problem, (macro) will output the average metrics of all classes, (all) will separately output the metrics of each class.')
+    parser.add_argument('--pretrain', default = 'base', help = 'Pretrain bert version: large, base')
+    parser.add_argument('--model_type', default = 'HMN', help = 'Use which model to do the task.(HMN, Bert_p: Parent label classifier, Bert_c: Child label classifier)')
     opt = parser.parse_args()
 
     kf = KFold(n_splits=opt.kf)
@@ -58,7 +59,10 @@ def main():
         print("K-fold:{}".format(kf_index))
 
         kf_index += 1
-        model = F_HMN(opt)
+        if opt.model_type == 'HMN':
+            model = F_HMN(opt)
+        else:
+            model = bertModel(opt)
         if opt.resume is not None:
             print("\nLoading model from {}...".format(opt.resume))
             model.load_state_dict(torch.load(opt.resume))
@@ -78,20 +82,27 @@ def main():
                 if opt.cuda == True:
                     text, parent_label, child_label= text.cuda(), parent_label.cuda(), child_label.cuda()
                 optimizer.zero_grad()
-                parent_prob, child_prob, b_nf_index, b_f_index = model(text, child_label_des, parent_label)
-                loss_parent = torch.nn.functional.binary_cross_entropy_with_logits(parent_prob, parent_label)
-                loss_child = 0
-                # NF child label loss, child_prob[0] shape: (m, 11)
-                if len(child_prob[0]) > 0:
 
-                    loss_child += torch.nn.functional.binary_cross_entropy_with_logits(child_prob[0], child_label[b_nf_index,:opt.clabel_nb - 1])
-                # F loss, child_prob[1] shape: (n, 1). m + n = bs
-                # if len(child_prob[1]) > 0:
-                #     loss_child += torch.nn.functional.binary_cross_entropy_with_logits(child_prob[1], child_label[b_f_index,11:])
+                if opt.model_type =='HMN':
+                    parent_prob, child_prob, b_nf_index, b_f_index = model(text, child_label_des, parent_label)
+                    loss_parent = torch.nn.functional.binary_cross_entropy_with_logits(parent_prob, parent_label)
+                    loss_child = 0
+                    # NF child label loss, child_prob[0] shape: (m, 11)
+                    if len(child_prob[0]) > 0:
 
-                    loss = loss_parent + loss_child
+                        loss_child += torch.nn.functional.binary_cross_entropy_with_logits(child_prob[0], child_label[b_nf_index,:opt.clabel_nb - 1])
+                    # F loss, child_prob[1] shape: (n, 1). m + n = bs
+                    # if len(child_prob[1]) > 0:
+                    #     loss_child += torch.nn.functional.binary_cross_entropy_with_logits(child_prob[1], child_label[b_f_index,11:])
+
+                        loss = loss_parent + loss_child
+                    else:
+                        loss = loss_parent
+                elif opt.model_type =='Bert_c':
+                    child_prob = model(text)
+                    loss = torch.nn.functional.binary_cross_entropy_with_logits(child_prob, child_label)
                 else:
-                    loss = loss_parent
+                    pass
                 loss_av += loss.item()
                 sum += 1
                 loss.backward()
@@ -110,69 +121,87 @@ def main():
                     text, parent_label, child_label = data
                     if opt.cuda == True:
                         text, parent_label, child_label= text.cuda(), parent_label.cuda(), child_label.cuda()
+                    if opt.model_type == 'HMN':
+                        parent_prob, child_prob = model(text, child_label_des, parent_label, mode = 'eval')
 
-                    parent_prob, child_prob = model(text, child_label_des, parent_label, mode = 'eval')
+                        parent_prob_sum_g.append(parent_label.cpu().squeeze(0).numpy())
+                        # if parent_label.cpu().squeeze(0).numpy()[0] == 1:
+                        child_prob_sum_g.append(child_label.cpu().squeeze(0).numpy())
 
-                    parent_prob_sum_g.append(parent_label.cpu().squeeze(0).numpy())
-                    # if parent_label.cpu().squeeze(0).numpy()[0] == 1:
-                    child_prob_sum_g.append(child_label.cpu().squeeze(0).numpy())
+                        loss_parent = torch.nn.functional.binary_cross_entropy_with_logits(parent_prob, parent_label)
+                        p_p = [0.] * 2
+                        p_p[parent_prob.cpu().squeeze(0).argmax(0)] = 1
+                        parent_prob_sum.append(p_p)
+                        loss_child = 0.
 
-                    loss_parent = torch.nn.functional.binary_cross_entropy_with_logits(parent_prob, parent_label)
-                    p_p = [0.] * 2
-                    p_p[parent_prob.cpu().squeeze(0).argmax(0)] = 1
-                    parent_prob_sum.append(p_p)
-                    loss_child = 0.
-
-                    # NFR
-                    if parent_prob.squeeze()[0] > parent_prob.squeeze()[1]:
+                        # NFR
+                        if parent_prob.squeeze()[0] > parent_prob.squeeze()[1]:
+                            c_p = [0.] * opt.clabel_nb
+                            loss_child += torch.nn.functional.binary_cross_entropy_with_logits(child_prob, child_label.squeeze()[:opt.clabel_nb - 1].unsqueeze(0))
+                            c_p[child_prob.cpu().squeeze(0).argmax(0)] = 1
+                        #else:
+                        # F
+                        #     loss_child = 0
+                        #     c_p[11] = 1
+                            child_prob_sum.append(c_p)
+                        else:
+                            c_p = [0.] * opt.clabel_nb
+                            c_p[-1] = 1
+                            child_prob_sum.append(c_p)
+                        loss = loss_parent + loss_child
+                    elif opt.model_type == 'Bert_c':
                         c_p = [0.] * opt.clabel_nb
-                        loss_child += torch.nn.functional.binary_cross_entropy_with_logits(child_prob, child_label.squeeze()[:opt.clabel_nb - 1].unsqueeze(0))
+                        child_prob = model(text)
                         c_p[child_prob.cpu().squeeze(0).argmax(0)] = 1
-                    #else:
-                    # F
-                    #     loss_child = 0
-                    #     c_p[11] = 1
                         child_prob_sum.append(c_p)
+                        child_prob_sum_g.append(child_label.cpu().squeeze(0).numpy())
+                        loss = torch.nn.functional.binary_cross_entropy_with_logits(child_prob, child_label)
+
                     else:
-                        c_p = [0.] * opt.clabel_nb
-                        c_p[-1] = 1
-                        child_prob_sum.append(c_p)
-                    loss = loss_parent + loss_child
+                        pass
+
                     loss_av += loss.item()
                     sum += 1
                 loss_av /= sum
+
                 parent_prob_sum = np.array(parent_prob_sum)
                 child_prob_sum = np.array(child_prob_sum)
                 parent_prob_sum_g = np.array(parent_prob_sum_g)
                 child_prob_sum_g = np.array(child_prob_sum_g)
 
-                (c_p, c_r, c_f1), c_acc = cal_metric(child_prob_sum_g, child_prob_sum, opt.metric)
-                (p_p, p_r, p_f1), p_acc = cal_metric(parent_prob_sum_g, parent_prob_sum, opt.metric)
-                (sma_p, sma_r, sma_f1), sacc = cal_metric(np.concatenate((child_prob_sum_g, parent_prob_sum_g), 1), np.concatenate((child_prob_sum, parent_prob_sum), 1))
-                if opt.metric == 'macro':
-                    print("Parent label : Precision: {} | Recall: {} | F1: {} | Acc : {}".format(p_p, p_r, p_f1, p_acc))
-                    print("Child label : Precision: {} | Recall: {} | F1: {} | Acc : {}".format(c_p, c_r, c_f1, c_acc))
-                    print("Summary : Precision: {} | Recall: {} | F1: {} | Acc : {}".format(sma_p, sma_r, sma_f1, sacc))
-                    print("Evaluation Loss:{}".format(loss_av))
-                    if sma_f1 > best_f1:
-                        best_f1 = sma_f1
-                        torch.save(model.state_dict(), os.path.join(opt.ckpt, ''.join([opt.id, '_', str(epoch), '_', str(sma_f1), '.pt'])))
-                else:
-                    print("Parent label :")
-                    # print(p_p, p_r, p_f1)
 
-                    #
+                (p_p, p_r, p_f1), p_acc = cal_metric(parent_prob_sum_g, parent_prob_sum, 'macro')
+                # (sma_p, sma_r, sma_f1), sacc = cal_metric(np.concatenate((child_prob_sum_g, parent_prob_sum_g), 1), np.concatenate((child_prob_sum, parent_prob_sum), 1))
+
+                if opt.model_type != 'Bert_c':
+                    print("Parent label Summary : Precision: {} | Recall: {} | F1: {} | Acc : {}".format(p_p, p_r, p_f1, p_acc))
+                    print("Parent label per class :")
+                    (p_p, p_r, p_f1), p_acc = cal_metric(parent_prob_sum_g, parent_prob_sum, None)
                     print("NFR: Precision: {} | Recall: {} | F1: {}".format(p_p[0], p_r[0], p_f1[0]))
                     print("F: Precision: {} | Recall: {} | F1: {}".format(p_p[1], p_r[1], p_f1[1]))
                     print("ACC:{}".format(p_acc))
-                    print("Child label :")
-                    for ind, name in enumerate(dataset.label_names[2:opt.clabel_nb + 1]):
-                        print('{}: Precision: {} | Recall: {} | F1: {}'.format(name, c_p[ind], c_r[ind], c_f1[ind]))
-                    print("Child label Accuracy:{}".format(c_acc))
-                    # print("Summary : Precision: {} | Recall: {} | F1: {} | Acc : {}".format(sma_p, sma_r, sma_f1, sacc))
-                    print("Evaluation Loss:{}".format(loss_av))
+                print()
+                (c_p, c_r, c_f1), c_acc = cal_metric(child_prob_sum_g, child_prob_sum, 'macro')
+                print("Child label Summary: Precision: {} | Recall: {} | F1: {} | Acc : {}".format(c_p, c_r, c_f1, c_acc))
+                #print("Summary : Precision: {} | Recall: {} | F1: {} | Acc : {}".format(sma_p, sma_r, sma_f1, sacc))
+                if c_f1 > best_f1:
+                    best_f1 = c_f1
+                    torch.save(model.state_dict(),
+                               os.path.join(opt.ckpt, ''.join([opt.id, '_', str(epoch), '_', str(c_f1), '.pt'])))
+
+                print("Child label per class :")
+                (c_p, c_r, c_f1), c_acc = cal_metric(child_prob_sum_g, child_prob_sum, None)
+                for ind, name in enumerate(dataset.label_names[2:opt.clabel_nb + 1]):
+                    print('{}: Precision: {} | Recall: {} | F1: {}'.format(name, c_p[ind], c_r[ind], c_f1[ind]))
+                print('{}: Precision: {} | Recall: {} | F1: {}'.format('F', c_p[4], c_r[4], c_f1[4]))
+                print("Child label Accuracy:{}".format(c_acc))
+                # print("Summary : Precision: {} | Recall: {} | F1: {} | Acc : {}".format(sma_p, sma_r, sma_f1, sacc))
+
+                print("Evaluation Loss:{}".format(loss_av))
+
             if (epoch) % 10 == 0:
                 adjust_learning_rate(optimizer)
+        print('Best f1:{}'.format(best_f1))
 
 def adjust_learning_rate(optimizer, decay_rate=.9):
     for param_group in optimizer.param_groups:
